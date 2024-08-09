6eb6b935218ba756f43a31b57346b76f229ff79b
wxw9868 <wxw9868@163.com>
2024-08-09 19:00:24 +0800

    .

--- src/components/stock/StockLiquidation.vue
+++ src/components/stock/StockLiquidation.vue
@@ -7,105 +7,76 @@
   ></v-data-table>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        headers: [
-          {
-            title: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            key: 'name',
-          },
-          { title: 'Calories', key: 'calories' },
-          { title: 'Fat (g)', key: 'fat' },
-          { title: 'Carbs (g)', key: 'carbs' },
-          { title: 'Protein (g)', key: 'protein' },
-          { title: 'Iron (%)', key: 'iron' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 200,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: 1,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 200,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: 1,
-          },
-          {
-            name: 'Eclair',
-            calories: 300,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: 7,
-          },
-          {
-            name: 'Cupcake',
-            calories: 300,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: 8,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 400,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: 16,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 400,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: 0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 400,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: 2,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 400,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: 45,
-          },
-          {
-            name: 'Donut',
-            calories: 500,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: 22,
-          },
-          {
-            name: 'KitKat',
-            calories: 500,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: 6,
-          },
-        ],
-      }
-    },
-  }
+import { ref, inject } from 'vue';
+import { useGoTo } from 'vuetify';
+
+export default {
+  setup() {
+    const host = inject('serverHost');
+    const goTo = useGoTo()
+    return { host, goTo }
+  },
+  data () {
+    return {
+      page: 1,
+      page_size: 100,
+      headers: [
+        {
+          title: 'ID',
+          align: 'start',
+          sortable: false,
+          key: 'id',
+        },
+        { title: '股票名称', key: 'stockName' },
+        { title: '盈亏', key: 'profitAndLoss' },
+        { title: '盈亏金额', key: 'amountOfMoney' },
+        { title: '收益率', key: 'earningRate' },
+        { title: '持股天数', key: 'holdStockDays' },
+        { title: '交易税费', key: 'transactionTaxes' },
+        { title: '建仓日期', key: 'buyingDay' },
+        { title: '清仓日期', key: 'sellingDay' },
+      ],
+      desserts: [],
+    }
+  },
+  methods: {
+    getData() {
+      // 向给定ID的用户发起请求
+      this.$http.get('/stock/liquidation', { params: {page: this.page, page_size: this.page_size} })
+        .then(response => {
+          // 处理成功情况
+          console.log(response);
+          if (response) {
+            this.desserts = response.data.data;
+          }
+        })
+        .catch(function (error) {
+          // 处理错误情况
+          if (error.response) {
+            // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // 请求已经成功发起，但没有收到响应
+            // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
+            // 而在node.js中是 http.ClientRequest 的实例
+            console.log(error.request);
+          } else {
+            // 发送请求时出了点问题
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+          console.log(error);
+        })
+        .finally(function () {
+          // 总是会执行
+          console.log("总是会执行");
+        });
+      },
+  },
+  mounted() {
+    this.getData();
+  },
+}
 </script>
--- src/components/stock/StockTradingRecords.vue
+++ src/components/stock/StockTradingRecords.vue
@@ -1,32 +1,128 @@
 <template>
+  <v-file-input
+    label="Import Excel"
+    placeholder="Import Excel"
+    prepend-icon="mdi-file-excel"
+    counter
+    chips
+    show-size
+    v-model="files"
+    v-on:change="uploadFile()"
+  ></v-file-input>
   <v-data-table
     :headers="headers"
     :items="desserts"
+    density="comfortable"
     :sort-by="[{ key: 'calories', order: 'asc' }, { key: 'fat', order: 'desc' }]"
     multi-sort
     loading-text="Loading... Please wait"
-    loading
+    :loading="loading"
   ></v-data-table>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        headers: [
-          {
-            title: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            key: 'name',
-          },
-          { title: 'Calories', key: 'calories' },
-          { title: 'Fat (g)', key: 'fat' },
-          { title: 'Carbs (g)', key: 'carbs' },
-          { title: 'Protein (g)', key: 'protein' },
-          { title: 'Iron (%)', key: 'iron' },
-        ],
-        desserts: [],
-      }
+import { ref, inject } from 'vue';
+import { useGoTo } from 'vuetify';
+
+export default {
+  setup() {
+    const host = inject('serverHost');
+    const goTo = useGoTo()
+    return { host, goTo }
+  },
+  data () {
+    return {
+      files: [],
+      page: 1,
+      page_size: 1000,
+      loading: true,
+      headers: [
+        {
+          title: 'ID',
+          align: 'start',
+          sortable: false,
+          key: 'id',
+        },
+        { title: '成交日期', key: 'trading_day' },
+        { title: '成交时间', key: 'trading_time' },
+        { title: '证券代码', key: 'securities_code' },
+        { title: '证券名称', key: 'securities_name' },
+        { title: '操作', key: 'operate' },
+        { title: '成交数量', key: 'trading_volume' },
+        { title: '成交均价', key: 'trading_average_price' },
+        { title: '成交金额', key: 'turnover' },
+        { title: '合同编号', key: 'contract_number' },
+        { title: '成交编号', key: 'trading_number' },
+        { title: '手续费', key: 'commission' },
+        { title: '印花税', key: 'stamp_duty' },
+        { title: '其他杂费', key: 'other_expenses' },
+        { title: '备注', key: 'note' },
+        { title: '股东帐户', key: 'shareholder_accounts' },
+      ],
+      desserts: [],
+    }
+  },
+  methods: {
+    getData() {
+      // 向给定ID的用户发起请求
+      this.$http.get('/stock/trading_records', { params: {page: this.page, page_size: this.page_size} })
+        .then(response=> {
+          // 处理成功情况
+          // console.log(response);
+          if (response) {
+            this.desserts = response.data.data;
+            this.loading = false
+          }
+        })
+        .catch(function (error) {
+          // 处理错误情况
+          if (error.response) {
+            // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // 请求已经成功发起，但没有收到响应
+            // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
+            // 而在node.js中是 http.ClientRequest 的实例
+            console.log(error.request);
+          } else {
+            // 发送请求时出了点问题
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+          console.log(error);
+        });
     },
-  }
+  
+    uploadFile() {
+      const formData = new FormData();
+      formData.append('file', this.files[0]);
+      this.$http.post('/stock/import_trading_records', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // 请求成功发出且服务器也响应了状态码，但状态代码超出了 2xx 的范围
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // 请求已经成功发起，但没有收到响应
+            // `error.request` 在浏览器中是 XMLHttpRequest 的实例，
+            // 而在node.js中是 http.ClientRequest 的实例
+            console.log(error.request);
+          } else {
+            // 发送请求时出了点问题
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+}
 </script>

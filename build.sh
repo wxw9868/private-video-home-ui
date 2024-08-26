15ccaca6928c7bf59d85df7c14e1b55ec4fe2f48
wxw9868 <wxw9868@163.com>
2024-08-27 03:51:13 +0800

    .

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-docker build -t my-video-ui:v1 .
+npm run build
+
+docker build -t my-video-ui:v1.0.1 .
 
 # Docker容器在后台运行
-docker run --rm -t -i -d -d -p 80:80 --name my-video-ui my-video-ui:v1
+docker run --rm -t -i -d -p 80:80 --name my-video-ui my-video-ui:v1
 
-docker run -d -d -p 80:80 --name my-video-ui my-video-ui:v1
+docker run -d -p 80:80 --name my-video-ui my-video-ui:v1
+
+docker run -d -p 80:80 --name my-video-ui-v1.0.1 my-video-ui:v1.0.1
